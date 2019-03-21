0f29033deffbbc27d45d5af7070f06688ce4aff5
James Ko <jamesqko@gmail.com>
2019-03-20 22:46:39 -0400

    fix issue with indexing keys in grid_search

--- grid_search.py
+++ grid_search.py
@@ -1,9 +1,9 @@
-def _grid_search_iter(grid, keys, values, index):
-    if index == len(grid):
+def _grid_search_iter(keys, values, index):
+    if index == len(keys):
         return
 
     key, param_values = keys[index], values[index]
-    base_iter = _grid_search_iter(grid, keys, values, index + 1)
+    base_iter = _grid_search_iter(keys, values, index + 1)
     for base_params in base_iter:
         for param_value in param_values:
             params = base_params.copy()
@@ -11,4 +11,6 @@ def _grid_search_iter(grid, keys, values, index):
             yield params
 
 def grid_search_iter(grid):
-    return _grid_search_iter(grid, grid.keys(), grid.values(), index=0)
+    return _grid_search_iter(keys=list(grid.keys()),
+                             values=list(grid.values()),
+                             index=0)
