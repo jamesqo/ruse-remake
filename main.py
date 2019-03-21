4a4bbb83fd48ef6241b741bbf77909f413f80754
James Ko <jamesqko@gmail.com>
2019-03-20 22:57:24 -0400

    separate classes into their own files, clean up imports

--- main.py
+++ main.py
@@ -1,125 +1,26 @@
 from os import path
-from typing import Iterator, List, Dict
 import sys
 
-import numpy as np
+from allennlp.common.file_utils import cached_path
+from allennlp.data.iterators import BucketIterator
+from allennlp.data.vocabulary import Vocabulary
+from allennlp.modules.elmo import Elmo
+from allennlp.modules.seq2vec_encoders.pytorch_seq2vec_wrapper import PytorchSeq2VecWrapper
+from allennlp.training.trainer import Trainer
+
 import torch
 import torch.optim as optim
-from torch.utils.data import random_split
-from torch.utils.data.dataset import Subset
-
-from allennlp.common.file_utils import cached_path
-from allennlp.data import Instance
-from allennlp.data.dataset_readers import DatasetReader
-from allennlp.data.fields import ArrayField, MetadataField, TextField, SequenceLabelField
-from allennlp.data.iterators import BucketIterator
-from allennlp.data.token_indexers import TokenIndexer, SingleIdTokenIndexer
-from allennlp.data.tokenizers import Token
-from allennlp.data.vocabulary import Vocabulary
-from allennlp.models import Model
-from allennlp.modules.elmo import Elmo, ELMoTokenCharactersIndexer
-from allennlp.modules.text_field_embedders import TextFieldEmbedder, BasicTextFieldEmbedder
-from allennlp.modules.token_embedders import Embedding
-from allennlp.modules.seq2vec_encoders.pytorch_seq2vec_wrapper import Seq2VecEncoder, PytorchSeq2VecWrapper
-from allennlp.nn.util import get_text_field_mask, sequence_cross_entropy_with_logits
-from allennlp.training.metrics import Covariance, CategoricalAccuracy, PearsonCorrelation
-from allennlp.training.trainer import Trainer
-from allennlp.predictors import SentenceTaggerPredictor
+import numpy as np
 
 from embedders import ELMoTextFieldEmbedder
 from grid_search import grid_search_iter
 from kfold import StratifiedKFold
+from model import RuseModel
+from reader import WmtDatasetReader
 
 torch.manual_seed(1)
 
 ### TODO: Separate these classes out into new files
-
-class WmtDatasetReader(DatasetReader):
-    def __init__(self, token_indexers: Dict[str, TokenIndexer] = None) -> None:
-        super().__init__(lazy=False)
-        self.token_indexers = token_indexers or {"tokens": ELMoTokenCharactersIndexer()}
-
-    def text_to_instance(self,
-                         mt_tokens: List[Token],
-                         ref_tokens: List[Token],
-                         human_score: float,
-                         origin: str) -> Instance:
-        mt_sent = TextField(mt_tokens, self.token_indexers)
-        ref_sent = TextField(ref_tokens, self.token_indexers)
-        human_score = ArrayField(np.array([human_score]))
-        origin = MetadataField(origin)
-
-        return Instance({"mt_sent": mt_sent,
-                         "ref_sent": ref_sent,
-                         "human_score": human_score,
-                         "origin": origin})
-
-    def _read(self, file_path: str) -> Iterator[Instance]:
-        with open(file_path, mode='r', encoding='utf-8') as file:
-            for line in file:
-                mt_text, ref_text, score_text, origin = line.strip().split('\t')
-                mt_words, ref_words, human_score = mt_text.split(), ref_text.split(), float(score_text)
-                yield self.text_to_instance(
-                        [Token(word) for word in mt_words],
-                        [Token(word) for word in ref_words],
-                        human_score,
-                        origin)
-
-class RuseModel(Model):
-    def __init__(self,
-                 word_embeddings: TextFieldEmbedder,
-                 encoder: Seq2VecEncoder,
-                 vocab: Vocabulary) -> None:
-        super().__init__(vocab)
-        self.word_embeddings = word_embeddings
-        self.encoder = encoder
-
-        hidden_dim = 128
-        self.mlp = torch.nn.Sequential(
-                torch.nn.Linear(in_features=encoder.get_output_dim()*4, out_features=hidden_dim),
-                torch.nn.Tanh(),
-                torch.nn.Linear(in_features=hidden_dim, out_features=hidden_dim),
-                torch.nn.Tanh(),
-                torch.nn.Linear(in_features=hidden_dim, out_features=1)
-            )
-        self.covar = Covariance()
-        self.pearson = PearsonCorrelation()
-
-    def forward(self,
-                mt_sent: Dict[str, torch.Tensor],
-                ref_sent: Dict[str, torch.Tensor],
-                human_score: np.ndarray,
-                origin: str) -> Dict[str, torch.Tensor]:
-        mt_mask = get_text_field_mask(mt_sent)
-        ref_mask = get_text_field_mask(ref_sent)
-
-        mt_embeddings = self.word_embeddings(mt_sent)
-        ref_embeddings = self.word_embeddings(ref_sent)
-
-        mt_encoder_out = self.encoder(mt_embeddings, mt_mask)
-        ref_encoder_out = self.encoder(ref_embeddings, ref_mask)
-    
-        input = torch.cat((mt_encoder_out,
-                           ref_encoder_out,
-                           torch.mul(mt_encoder_out, ref_encoder_out),
-                           torch.abs(mt_encoder_out - ref_encoder_out)), 1)
-        reg = self.mlp(input)
-        output = {"reg": reg}
-
-        if human_score is not None:
-            # run metric calculation
-            self.covar(reg, human_score)
-            self.pearson(reg, human_score)
-
-            # calculate mean squared error
-            delta = reg - human_score
-            output["loss"] = torch.mul(delta, delta).sum()
-
-        return output
-
-    def get_metrics(self, reset: bool = False) -> Dict[str, float]:
-        return {"covar": self.covar.get_metric(reset),
-                "pearson": self.pearson.get_metric(reset)}
 
 ### End TODO
 
---
+++ model.py
@@ -0,0 +1,67 @@
+from typing import Dict
+
+from allennlp.data.vocabulary import Vocabulary
+from allennlp.models import Model
+from allennlp.modules.seq2vec_encoders.pytorch_seq2vec_wrapper import Seq2VecEncoder
+from allennlp.modules.text_field_embedders import TextFieldEmbedder
+from allennlp.nn.util import get_text_field_mask
+from allennlp.training.metrics import Covariance, PearsonCorrelation
+
+import numpy as np
+import torch
+
+class RuseModel(Model):
+    def __init__(self,
+                 word_embeddings: TextFieldEmbedder,
+                 encoder: Seq2VecEncoder,
+                 vocab: Vocabulary) -> None:
+        super().__init__(vocab)
+        self.word_embeddings = word_embeddings
+        self.encoder = encoder
+
+        hidden_dim = 128
+        self.mlp = torch.nn.Sequential(
+                torch.nn.Linear(in_features=encoder.get_output_dim()*4, out_features=hidden_dim),
+                torch.nn.Tanh(),
+                torch.nn.Linear(in_features=hidden_dim, out_features=hidden_dim),
+                torch.nn.Tanh(),
+                torch.nn.Linear(in_features=hidden_dim, out_features=1)
+            )
+        self.covar = Covariance()
+        self.pearson = PearsonCorrelation()
+
+    def forward(self,
+                mt_sent: Dict[str, torch.Tensor],
+                ref_sent: Dict[str, torch.Tensor],
+                human_score: np.ndarray,
+                origin: str) -> Dict[str, torch.Tensor]:
+        mt_mask = get_text_field_mask(mt_sent)
+        ref_mask = get_text_field_mask(ref_sent)
+
+        mt_embeddings = self.word_embeddings(mt_sent)
+        ref_embeddings = self.word_embeddings(ref_sent)
+
+        mt_encoder_out = self.encoder(mt_embeddings, mt_mask)
+        ref_encoder_out = self.encoder(ref_embeddings, ref_mask)
+    
+        input = torch.cat((mt_encoder_out,
+                           ref_encoder_out,
+                           torch.mul(mt_encoder_out, ref_encoder_out),
+                           torch.abs(mt_encoder_out - ref_encoder_out)), 1)
+        reg = self.mlp(input)
+        output = {"reg": reg}
+
+        if human_score is not None:
+            # run metric calculation
+            self.covar(reg, human_score)
+            self.pearson(reg, human_score)
+
+            # calculate mean squared error
+            delta = reg - human_score
+            output["loss"] = torch.mul(delta, delta).sum()
+
+        return output
+
+    def get_metrics(self, reset: bool = False) -> Dict[str, float]:
+        return {"covar": self.covar.get_metric(reset),
+                "pearson": self.pearson.get_metric(reset)}
